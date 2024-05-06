6561872ad60eea4dcb7de8a313b817239fb8762a
Andy-Castle <masterchief_and@live.com>
2024-05-05 21:35:49 -0500

    ya quedo esto

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 const container = document.getElementById("container");
-let boxes = 16;
-let totalDivs = boxes * boxes;
-let boxeSizes = 35;
-let boxesArea = boxeSizes * 2;
-let sketchArea = boxesArea + totalDivs * 2;
+const colorButton = document.getElementById("changeColor");
+const normalButton = document.getElementById("normal");
+const sizeButton = document.getElementById("changeSize");
+const eraseButton = document.getElementById("erase");
 
-for (let i = 0; i < totalDivs; i++) {
-  const div = document.createElement("div");
-  div.id = `${i}`;
-  div.setAttribute("style", `width: ${boxeSizes}px; height: ${boxeSizes}px;`);
-  div.classList.add("etch");
-  container.appendChild(div);
-
-  div.addEventListener("click", () => {
-    div.classList.toggle("backgroundColorBlack");
+function blackWhite(div) {
+  div.addEventListener("mouseover", () => {
+    div.classList.add("backgroundColorBlack");
   });
 }
 
-container.setAttribute("style", `width: ${sketchArea}px;`);
+function eraser() {
+  var divs = document.querySelectorAll(".etch");
+  divs.forEach(function (div) {
+    div.classList.remove("backgroundColorBlack");
+  });
+}
+
+// function divRGB(div) {
+//   let red = Math.floor(Math.random() * 255);
+//   let blue = Math.floor(Math.random() * 255);
+//   let green = Math.floor(Math.random() * 255);
+//   div.setAttribute("style", `background: rgb(${red}, ${green}, ${blue})`);
+// }
+
+function makeBoard() {
+  container.innerHTML = "";
+  let boxes = prompt("Write a number bewteen 20 and 100");
+  if (boxes < 20 || boxes > 100 || boxes === NaN || boxes === undefined) {
+    alert("Please write a number bewteen 20 and 100");
+    writeBoard(20);
+  } else {
+    writeBoard(boxes);
+  }
+}
+
+function writeBoard(boxes) {
+  let boxesArea = 960;
+  let boxeSizes = boxesArea / boxes;
+  let totalDivs = boxes * boxes;
+  for (let i = 0; i < totalDivs; i++) {
+    const div = document.createElement("div");
+    div.id = `${i}`;
+    div.setAttribute("style", `width: ${boxeSizes}px; height: ${boxeSizes}px;`);
+    div.classList.add("etch");
+    container.appendChild(div);
+    blackWhite(div);
+  }
+}
+
+makeBoard();
+
+sizeButton.addEventListener("click", function () {
+  makeBoard();
+});
+
+eraseButton.addEventListener("click", function () {
+  eraser();
+});
